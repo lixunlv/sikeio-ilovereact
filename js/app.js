c35ce71036f2d9147ea5b2b1098eaed48f1caabc
lixunlv <lxunlv@163.com>
2015-10-03 17:17:15 +0800

    eight

--- js/app.js
+++ js/app.js
@@ -1,7 +1,66 @@
 window.onload = function () {
     animateLogo();
     animateRobot();
+    updateSliderControl();
+    addSmoothScrolling();
 };
+
+window.onscroll = function () {
+    updateSliderControl();
+};
+
+function updateSliderControl() {
+    // get all the slider links
+    var links = document.querySelectorAll("#slider-control a")
+
+    for (var i = 0; i < links.length; i++) {
+        var link = links[i];
+
+        // Get the section pointed to by the link
+        var section = document.querySelector(link.getAttribute('href'));
+        var sectionTop = section.offsetTop;
+        var sectionBottom = section.offsetTop + section.offsetHeight;
+
+        // Check if window.scrollY is between the section.
+        if (window.scrollY >= sectionTop && window.scrollY < sectionBottom) {
+            link.className = "active";
+        } else {
+            link.className = "";
+        }
+    }
+}
+
+function scrollToElement(element) {
+    var topOfElement = element.offsetTop;
+
+    TweenMax.to(window, 1, {
+        scrollTo: {
+            y: topOfElement,
+        },
+
+        ease: Power2.easeInOut,
+    });
+}
+
+function addSmoothScrolling() {
+    var links = document.querySelectorAll("#slider-control a");
+
+    for (var i = 0; i < links.length; i++) {
+        var link = links[i];
+
+        link.addEventListener("click", function (wrapLink) {
+            return function(event) {
+                // `event` is the mouse click event
+                event.preventDefault();
+
+                // BUG WARNING! Fix with a closure or ES6 `let`.
+                var href = wrapLink.getAttribute('href');
+
+                scrollToElement(document.querySelector(href));
+            };
+        }(link));
+    }
+}
 
 function animateLogo() {
     TweenMax.fromTo(".react-logo", 2, {
